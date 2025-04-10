6839f30e7314ab5ca2e7e785f2ad04a3f03c9c4d
Hiroshi <eduhito57@gmail.com>
2025-04-09 22:31:07 -0300

    feat: add NavLink customization

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -1,15 +1,42 @@
 import React from "react";
-import { Outlet, Link } from "react-router";
+import { Outlet, NavLink } from "react-router";
 
 import "./DashboardNav.css";
 
 export default function DashboardNav() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#202020"
+  }
+  
+  // end é usado para evitar que haja um highlight em dashboard (devido ao nesting de dashboard)
+
   return (
     <>
       <nav className="dashboard-nav">
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/dashboard/details">Details</Link>
-        <Link to="/dashboard/reviews">Reviews</Link>
+        <NavLink 
+          end
+          to="/dashboard"
+          style={({isActive}) => isActive ? activeStyles : null}
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink 
+          to="/dashboard/details"
+          style={({isActive}) => isActive ? activeStyles : null}
+        >
+          Details
+        </NavLink>
+
+        <NavLink 
+          to="/dashboard/reviews"
+          style={({isActive}) => isActive ? activeStyles : null}
+        >
+          Reviews
+        </NavLink>
+
       </nav>
       <Outlet/>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router"
+import { Link, NavLink } from "react-router"
 import './Header.css';
 
 export default function Header() {
@@ -6,9 +6,26 @@ export default function Header() {
     <header>
       <Link className="logo" to="/">BOOK archive</Link>
       <nav>
-        <Link className="links" to="/dashboard">Dashboard</Link>
-        <Link className="links" to="/about">About</Link>
-        <Link className="links" to="/books">Book list</Link>
+        <NavLink
+          className={({isActive}) => isActive ? "active-link" : null}
+          to="/dashboard"
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink
+          className={({isActive}) => isActive ? "active-link" : null}
+          to="/about"
+        >
+          About
+        </NavLink>
+
+        <NavLink
+          className={({isActive}) => isActive ? "active-link" : null}
+          to="/books"
+        >
+          Book list
+        </NavLink>
       </nav>
     </header>
   )
